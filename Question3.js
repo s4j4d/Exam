b454c7a73c894cd91836136d85558e61d0fd9333
s4j4d <47069604+s4j4d@users.noreply.github.com>
2022-08-26 19:59:48 +0430

    soal 3

---
+++ Question3.js
@@ -0,0 +1,17 @@
+
+function productSum(){
+    for(let i=2;i<=1200;i++){
+        
+    }
+}
+
+
+function maghsoomfinder(number){
+    const result =[]
+    for(let i=1;i<number/2;i++){
+        if(number%i == 0){
+            result.push(i)
+        }
+    }
+    return result
+}
